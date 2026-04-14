351106448e1321a0b53578c88f5f38aefd5235d3
Agam Chawla <215956979+agam-89@users.noreply.github.com>
2026-04-14 23:39:18 +0530

    Add centralized configuration parameters for the LFO Synthesis Harmonic Analyzer.

---
+++ utils/config.m
@@ -0,0 +1,6 @@
+% Configuration parameters for LFO Synthesis Harmonic Analyzer
+
+fs = 44100;                % Sampling frequency in Hz
+T = 1/fs;                   % Sampling period
+f0 = 100;                  % Fundamental frequency in Hz
+N_harmonics = 5;           % Number of harmonics to synthesize
